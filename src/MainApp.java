8f7a94fee99ff058031de052562d2174a933e891
RaduHandrugan <radu.handrugan@student.upt.ro>
2025-01-10 13:01:48 +0200

    Laboratorul2Pb4

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+/*
+4. Să se realizeze un program care citește numele si CNP-ul pe care îl au n persoane.
+Valoarea lui n se citește de la tastatură. Programul va afișa informațiile introduse și în plus
+pentru fiecare persoana va afișa vârsta. Cât timp un CNP-ul este introdus greșit programul va
+cere reintroducerea acestuia. Pentru simplitate se consideră că CNP-ul este valid dacă
+îndeplinește următoarele condiții:
+• Are 13 caractere
+• Toate caracterele sunt cifre
+• Prima cifră are una din valorile 1, 2, 5, 6
+• Cifra de control a CNP-ului are o valoare validă.
+Se va crea clasa Persoana cu variabile membre private nume (String) şi cnp (String).
+Clasa va avea constructor cu parametri, gettere si settere în funcție de necesități şi metoda
+getVarsta() care va calcula şi va returna vârsta persoanei extrăgând data nașterii din CNP şi
+citind din sistem data curentă. Se va utiliza clasa LocalDate. Se va crea un vector în care se
+vor adăuga obiectele de tip Persoana. Fiecare element din vectorul va fi afișat pe un rând în
+formatul nume, CNP, varsta.
+ */
 
-public class MainApp {
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+
+public class MainApp
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduceti numarul de persoane: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        Persoana[] persoane = new Persoana[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            System.out.print("Introduceti numele persoanei " + (i + 1) + ": ");
+            String nume = scanner.nextLine();
+
+            String cnp;
+            while (true)
+            {
+                System.out.print("Introduceti CNP-ul persoanei " + (i + 1) + ": ");
+                cnp = scanner.nextLine();
+                if (Persoana.esteCnpValid(cnp))
+                {
+                    break;
+                } else {
+                    System.out.println("CNP-ul NU e VALID!  ");
+                }
+            }
+
+            persoane[i] = new Persoana(nume, cnp);
+        }
+        
+        System.out.println("\nDetalii persoana: ");
+        for (Persoana persoana : persoane)
+        {
+            System.out.println(persoana.getNume() + ", " + persoana.getCnp() + ", Varsta: " + persoana.getVarsta() + " ani");
+        }
+
+        scanner.close();
+    }
 }
--- src/Persoana.java
+++ src/Persoana.java
@@ -1,4 +1,78 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
 
-public class Persoana {
+class Persoana
+{
+    private String nume;
+    private String cnp;
+
+    public Persoana(String nume, String cnp)
+    {
+        this.nume = nume;
+        this.cnp = cnp;
+    }
+
+    // Getter & setter
+    public String getNume()
+    {
+        return nume;
+    }
+    public void setNume(String nume)
+    {
+        this.nume = nume;
+    }
+
+
+
+    public String getCnp()
+    {
+        return cnp;
+    }
+    public void setCnp(String cnp)
+    {
+        this.cnp = cnp;
+    }
+
+
+    public int getVarsta()
+    {
+        int an = Integer.parseInt(cnp.substring(1, 3));         // ultimele 2 caractere pt an
+        int luna = Integer.parseInt(cnp.substring(3, 5));       // urm 2 caracatere pt luna
+        int zi = Integer.parseInt(cnp.substring(5, 7));            // urm 2 caractere ziua
+
+
+        int prefix = Integer.parseInt(cnp.substring(0, 1));             //prima cifra din cnp
+
+        if (prefix == 1 || prefix == 2)                             //verific pt ambele cazuri de varsta (nascut inainte/dupa 2000)
+        {
+            an += 1900;
+        } else if (prefix == 5 || prefix == 6)
+        {
+            an += 2000;
+        }
+
+        LocalDate dataNasterii = LocalDate.of(an, luna, zi);
+        LocalDate dataCurenta = LocalDate.now();               //data curenta (sysdate)
+
+
+        Period perioada = Period.between(dataNasterii, dataCurenta); // calculez varsta fata de anul curent (data curenta luata cu fct)
+        return perioada.getYears();
+    }
+
+    //validare CNP
+    public static boolean esteCnpValid(String cnp)
+    {
+        if (cnp.length() != 13)
+        {
+            return false;
+        }
+
+        char primaCifra = cnp.charAt(0);                     //verific ca prima cifra sa fie corecta
+        if (primaCifra != '1' && primaCifra != '2' && primaCifra != '5' && primaCifra != '6')
+        {
+            return false;
+        }
+        return true;
+    }
 }
